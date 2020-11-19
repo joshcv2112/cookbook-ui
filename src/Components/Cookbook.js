8288d5d0e4157e6b879bed52909586ed99fba2a6
joshcv2112 <joshcv2112@gmail.com>
2020-11-18 23:13:15 -0700

    looped RecipeCard component

--- src/Components/Cookbook.js
+++ src/Components/Cookbook.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import '../Style/style.css';
 import Loading from './Loading';
+import RecipeCard from './RecipeCard';
 
 class Cookbook extends Component {
   render() {
     return(
         <div className='main'>
-            { this.props.loading ?
-                (<p>{JSON.stringify(this.props.recipeData)}</p>) :
-                (<Loading />) }
+        { 
+          this.props.loading ?
+            (
+              this.props.recipeData.map(recipe => (
+                <RecipeCard recipe={recipe} />
+              ))
+            ) :
+            (<Loading />) 
+        }
         </div>
     );
   }
