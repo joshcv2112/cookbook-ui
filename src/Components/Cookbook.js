322b0ce64e5e4ef912dceb98678911cfc2f4e3bf
joshcv2112 <joshcv2112@gmail.com>
2020-11-29 22:07:07 -0700

    began implementation of mega sidebar

--- src/Components/Cookbook.js
+++ src/Components/Cookbook.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import '../Style/style.css';
-import Loading from './Loading';
-import RecipeCard from './RecipeCard';
 
 class Cookbook extends Component {
   render() {
     return(
-        <div>
-          <div>
-            
+      <div>
+        <div className='test'>
+          <div className='sidebar-title-container'>
+            <h2>Cookbook</h2>
           </div>
-          <div className='main'>
-          { 
-            this.props.loading ?
-              (
-                this.props.recipeData.map(recipe => (
-                  <RecipeCard recipe={recipe} />
-                ))
-              ) :
-              (<Loading />) 
-          }
+          <div className="dropdown">
+            <button className="dropbtn">Cookbook 1</button>
+            <div className="dropdown-content">
+              <a>Cookbook 1</a>
+              <a>Cookbook 2</a>
+              <a>Cookbook 3</a>
+            </div>
           </div>
         </div>
+        <div className='main-cookbook'>
+          <h2>Recipe</h2>
+        </div>
+      </div>
     );
   }
 }
