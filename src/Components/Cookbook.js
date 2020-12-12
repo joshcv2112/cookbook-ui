97b9fe8465f123e482a57d8a87fd07b6993a44aa
joshcv2112 <joshcv2112@gmail.com>
2020-12-11 23:45:47 -0800

    finished layout of recipe detail page

--- src/Components/Cookbook.js
+++ src/Components/Cookbook.js
@@ -65,6 +65,30 @@ class Cookbook extends Component {
                   <button className="add-note-button">Add Note</button>
                 </div>
               </div>
+              <div className="recipe-directions-region">
+                <div className="ingredients">
+                  <div className="ingredient"><b>6</b> dried chiles de árbol</div>
+                  <div className="ingredient"><b>1/3 cup</b> fish sauce</div>
+                  <div className="ingredient"><b>1 tablespoon</b> fresh lime juice</div>
+                  <div className="ingredient"><b>1 tablespoon</b> toasted sesame seeds</div>
+                  <div className="ingredient"><b>2 teaspoons</b> sugar</div>
+                  <div className="ingredient"><b>1/2 cup</b> oyster sauce</div>
+                  <div className="ingredient"><b>1/2 cup</b> Thai thin soy sauce</div>
+                  <div className="ingredient"><b>2 tablespoons</b> sugar</div>
+                  <div className="ingredient"><b>2 tablespoons</b> vegetable oil</div>
+                  <div className="ingredient"><b>2 teaspoons</b> freshly ground black pepper</div>
+                  <div className="ingredient"><b>2 pounds</b> chicken wings, tips removed, drumettes and flats separated</div>
+                  <div className="ingredient">A spice mill or a mortar and pestle; twelve 8" bamboo skewers soaked in water at least 1 hour</div>
+                </div>
+                <div className="directions">
+                  <div className="direction">
+                    <p>Dipping Sauce: Grind chiles in spice mill to a fine powder. Mix chile powder, fish sauce, lime juice, sesame seeds, and sugar in a medium bowl to dissolve sugar. Adjust with more sugar or lime juice if needed.</p>
+                  </div>
+                  <div className="direction">
+                    <p>Wings and Assembly: Prepare grill for medium-high heat. Combine oyster and soy sauces, sugar, oil, and pepper in a large bowl. Add wings; toss to coat. Let sit 20–30 minutes (if allowed to marinate longer, they’ll be too salty). Thread 2 flat pieces onto each skewer on a diagonal, spacing about ½" apart. Grill along with drumettes, turning occasionally, until lightly charred and cooked through, 6–8 minutes. Serve with dipping sauce.</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
