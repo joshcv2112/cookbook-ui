b765a7e14a869a8368114d98f5418675f4884699
joshcv2112 <joshcv2112@gmail.com>
2021-01-17 21:39:06 -0800

    dynamically list recipes

--- src/Components/Cookbook/RecipeSelector.js
+++ src/Components/Cookbook/RecipeSelector.js
@@ -6,7 +6,7 @@ class RecipeSelector extends React.Component {
         super(props);
 
         this.state = {
-            selectedSection : 1,
+            selectedRecipeNum : 1,
             loading: true,
             rawData: null,
             recipes: null
@@ -19,22 +19,35 @@ class RecipeSelector extends React.Component {
 
         var recipes = [];
         for (let i = 0; i < data.length; i++) {
-            recipes.push({name: data[i].title, style: "dropbtn"});
+            if (i === this.state.selectedRecipeNum)
+                recipes.push({name: data[i].title, style: "dropbtn-selected"});
+            else
+                recipes.push({name: data[i].title, style: "dropbtn"});
         }
 
         this.setState({loading: false, recipes: recipes, rawData: data})
     }
 
     async componentDidMount() {
-        await this.getRecipeData('/api/recipes/section/' + this.state.selectedSection);
+        await this.getRecipeData('/api/recipes/section/' + this.state.selectedRecipeNum);
+    }
+
+    updateRecipeListStyle(name) {
+        var recipes = [];
+        for (let i = 0; i < this.state.rawData.length; i++) {
+            if (name === this.state.rawData[i].title)
+                recipes.push({name: this.state.rawData[i].title, style: "dropbtn-selected"});
+            else
+                recipes.push({name: this.state.rawData[i].title, style: "dropbtn"});
+        }
+        return recipes;
     }
 
     updateRecipeStyle(name) {
-        
+        this.setState({recipes: this.updateRecipeListStyle(name), loading: false});
     }
 
     renderRecipes() {
-        // return <button className="dropbtn">Done Loading</button>;
         return <div>
             {this.state.recipes.map(recipe => (
                 <button className={recipe.style} key={recipe.name} onClick={this.updateRecipeStyle.bind(this, recipe.name)}>{recipe.name}</button>
@@ -47,9 +60,6 @@ class RecipeSelector extends React.Component {
             <div className="two">
                 {this.state.loading ? <button className="dropbtn">Loading...</button> : 
                     this.renderRecipes()}
-                <button className="dropbtn">{this.state.selectedSection}</button>
-                <button className="dropbtn">Recipe 1</button>
-                <button className="dropbtn-selected">Recipe 2</button>
                 <div className="add-section-button">Add Recipe</div>
             </div>
         );
